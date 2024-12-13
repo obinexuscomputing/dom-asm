f16187a0ddd64417272d570530f58280cc91480a
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-13 22:41:18 +0000

    int

--- packages/dom-css/dist/index.d.ts
+++ packages/dom-css/dist/index.d.ts
@@ -59,4 +59,39 @@ declare class Validator {
     validate(): string[];
 }
 
-export { ASTBuilder, type ASTNode, Optimizer, type Token, Tokenizer, Validator };
+/**
+ * import { Tokenizer } from "../tokenizer";
+import { ASTBuilder } from "../ast";
+import { Optimizer } from "../optimizer";
+
+const cssInput = `/* Example CSS \*\/
+body {
+    background: white;
+    color: black;
+    color: black;
+  }
+  
+  const tokenizer = new Tokenizer(cssInput);
+  const tokens = tokenizer.tokenize();
+  const astBuilder = new ASTBuilder(tokens);
+  let ast = astBuilder.buildAST();
+  
+  const optimizer = new Optimizer(ast);
+  ast = optimizer.optimize();
+  
+  const generator = new CodeGenerator(ast);
+  const cssOutput = generator.generate();
+  
+  console.log(cssOutput);
+  **/
+declare class CodeGenerator {
+    private ast;
+    constructor(ast: ASTNode);
+    private generateStylesheet;
+    private generateRule;
+    private generateSelector;
+    private generateDeclaration;
+    generate(): string;
+}
+
+export { ASTBuilder, type ASTNode, CodeGenerator, Optimizer, type Token, Tokenizer, Validator };
