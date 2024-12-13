52ed2478e03ba7300d1ee5501232c84558a5f267
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-13 22:31:48 +0000

    init

--- packages/dom-html/src/parser/index.ts
+++ packages/dom-html/src/parser/index.ts
@@ -2,6 +2,35 @@ import { HTMLTokenizer } from "../tokenizer/index";
 import { AST, ASTNode } from "../ast/index";
 import { ValidationResult, Validator } from "../validator/index";
 
+/**
+ * import { Parser } from "./parser/index";
+
+const htmlInput = `
+<html:html>
+  <html:head>
+    <html:title>Sample HTML6 Document</html:title>
+  </html:head>
+  <html:body>
+    <custom:widget id="main-widget" type="interactive"></custom:widget>
+  </html:body>
+</html:html>
+`;
+
+const parser = new Parser();
+const { ast, validationResult } = parser.parse(htmlInput);
+
+if (validationResult.valid) {
+  console.log("Valid HTML6 Document.");
+} else {
+  console.error("Validation Errors:");
+  validationResult.errors.forEach((error) => console.error(error));
+}
+
+// Optionally, visualize the AST
+const astBuilder = new AST();
+astBuilder.printAST(ast);
+
+ */
 class Parser {
   private tokenizer: HTMLTokenizer;
   private astBuilder: AST;
