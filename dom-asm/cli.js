a88766a677c1dbf7600d03f5cec03288e53ab8d4
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-13 21:24:10 +0000

    it

---
+++ dom-asm/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const minimist = require('minimist');
+const chalk = require('chalk');
+
+const args = minimist(process.argv.slice(2));
+console.log(chalk.green("dom-asm CLI running with args:"), args);
