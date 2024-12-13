29b2433967d4e4801def9f006cf525163041d444
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-13 22:27:55 +0000

    init

--- packages/dom-html/src/validator/index.ts
+++ packages/dom-html/src/validator/index.ts
@@ -5,6 +5,41 @@ type ValidationResult = {
   errors: string[];
 };
 
+/**
+ * import { HTMLTokenizer } from "./tokenizer/index";
+import { AST } from "./ast/index";
+import { Validator } from "./validator/index";
+
+const htmlInput = `
+<html:html>
+  <html:head>
+    <html:title>Sample HTML6 Document</html:title>
+  </html:head>
+  <html:body>
+    <html:media src="logo.png" type="image" />
+    <html:p>This is a sample document.</html:p>
+    <invalid:tag>Oops!</invalid:tag>
+  </html:body>
+</html:html>
+`;
+
+const tokenizer = new HTMLTokenizer(htmlInput);
+const tokens = tokenizer.tokenize();
+
+const astBuilder = new AST();
+const ast = astBuilder.buildAST(tokens);
+
+const validator = new Validator();
+const validationResult = validator.validateAST(ast);
+
+if (validationResult.valid) {
+  console.log("The document is valid.");
+} else {
+  console.error("Validation errors:");
+  validationResult.errors.forEach((error) => console.error(error));
+}
+
+ */
 class Validator {
   private namespaceRules: Record<string, string[]> = {
     "html": ["html", "head", "body", "title", "meta", "link", "p", "div", "a", "img"],
