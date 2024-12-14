44de2e3288756d14876882ad55bdd5ba6a7d93f7
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-14 14:20:45 +0000

    init

--- packages/css/index.ts
+++ packages/css/index.ts
@@ -1,1 +1,32 @@
-export * from './src/index'
+
+// Re-export modules for public API
+export { Tokenizer } from "./tokenizer";
+export { ASTBuilder, ASTNode } from "./ast";
+export { Validator } from "./validator";
+export { Optimizer } from "./optimizer";
+export { CodeGenerator } from "./generator";
+export { Parser } from "./parser";
+
+// UMD module initialization (if required for global use)
+if (typeof window !== "undefined") {
+  (window as any).DOMCSS = {
+    Tokenizer: Tokenizer,
+    ASTBuilder: ASTBuilder,
+    Validator: Validator,
+    Optimizer: Optimizer,
+    CodeGenerator: CodeGenerator,
+    Parser: Parser,
+  };
+}
+
+// Example of usage in UMD
+/**
+<script src="path-to-dom-css.umd.js"></script>
+<script>
+  const tokenizer = new DOMCSS.Tokenizer("body { color: red; }");
+  const tokens = tokenizer.tokenize();
+  const astBuilder = new DOMCSS.ASTBuilder(tokens);
+  const ast = astBuilder.buildAST();
+  console.log(ast);
+</script>
+**/
