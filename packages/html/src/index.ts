e6b95132dce80465a3f66d510e78988f53325257
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-14 21:57:25 +0000

    itnc

--- packages/html/src/index.ts
+++ packages/html/src/index.ts
@@ -11,5 +11,4 @@ export { AST, ASTNode } from "./ast";
 export { Validator } from "./validator";
 export { ASTOptimizer as Optimizer } from "./optimizer";
 export { CodeGenerator } from "./generator";
-export { Parser } from "./parser";
-export { ParserError } from './parser/ParserError';
+export { Parser, ParserError} from "./parser";
