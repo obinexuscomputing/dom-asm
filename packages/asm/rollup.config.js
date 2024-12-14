e39f51e52b8ab88100a9c7895d0a89fff3bc99b7
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-14 12:38:37 +0000

    int

--- packages/asm/rollup.config.js
+++ packages/asm/rollup.config.js
@@ -9,12 +9,12 @@ export default [
     input: 'src/index.ts',
     output: [
       {
-        file: 'dist/index.cjs.js',
+        file: 'dist/index.cjs',
         format: 'cjs',
         sourcemap: true,
       },
       {
-        file: 'dist/index.esm.js',
+        file: 'dist/index.js',
         format: 'esm',
         sourcemap: true,
       },
