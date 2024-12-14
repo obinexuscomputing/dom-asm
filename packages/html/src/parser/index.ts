7fbc847b3e07210836e34ae77adaa347b1addd5b
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-14 21:56:48 +0000

    init

--- packages/html/src/parser/index.ts
+++ packages/html/src/parser/index.ts
@@ -1,9 +1,24 @@
 import { Token, HTMLTokenizer } from "../tokenizer/index";
 import { AST, ASTNode } from "../ast/index";
 import { ValidationResult, Validator } from "../validator/index";
-import { ParserError } from "./ParserError";
-
 type ErrorHandler = (error: ParserError) => void;
+
+export class ParserError extends Error {
+  token: Token;
+  position: number;
+
+  constructor(message: string, token: Token, position: number) {
+    super(message);
+    this.name = "ParserError";
+    this.token = token;
+    this.position = position;
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ParserError);
+    }
+  }
+}
+
 
 export class Parser {
   private tokenizer: HTMLTokenizer;
