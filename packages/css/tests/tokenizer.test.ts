bf7c9797cfbba949f2e9d7e132e81a5e516dc6e2
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-14 21:25:29 +0000

    it

--- packages/css/tests/tokenizer.test.ts
+++ packages/css/tests/tokenizer.test.ts
@@ -8,12 +8,12 @@ describe("Tokenizer", () => {
 
     expect(tokens).toEqual([
       { type: "other", value: "body", position: { line: 1, column: 5 } },
-      { type: "other", value: "{", position: { line: 1, column: 7 } },
+      { type: "delimiter", value: "{", position: { line: 1, column: 7 } },
       { type: "other", value: "color", position: { line: 1, column: 13 } },
-      { type: "other", value: ":", position: { line: 1, column: 14 } },
-      { type: "other", value: "black", position: { line: 1, column: 19 } },
-      { type: "other", value: ";", position: { line: 1, column: 20 } },
-      { type: "other", value: "}", position: { line: 1, column: 21 } },
+      { type: "delimiter", value: ":", position: { line: 1, column: 14 } },
+      { type: "other", value: "black", position: { line: 1, column: 20 } },
+      { type: "delimiter", value: ";", position: { line: 1, column: 21 } },
+      { type: "delimiter", value: "}", position: { line: 1, column: 23 } },
     ]);
   });
 });
