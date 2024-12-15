4f88fa0f02b7f0d32fd50120c069f47d6b8844fd
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 22:26:15 +0000

    int

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './cli/';
 export * from "@obinexuscomputing/css";
 export * from "@obinexuscomputing/html";
 export * from "@obinexuscomputing/js";
+export * from '@obinexuscomputing/xml';
