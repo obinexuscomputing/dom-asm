bec59083b10ddd8af8289c4a2a549285a5912b1b
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 05:06:00 +0000

    it

---
+++ packages/html/dist/generator/index.d.ts
@@ -0,0 +1,8 @@
+import { ASTNode } from "../ast/index";
+declare class CodeGenerator {
+    generateHTML(node: ASTNode): string;
+    private generateAttributes;
+    private isSelfClosingTag;
+}
+export { CodeGenerator };
+//# sourceMappingURL=index.d.ts.map
