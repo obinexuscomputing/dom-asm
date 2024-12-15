93e6577d3a8173e3518c3c688453146475a482be
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 23:26:57 +0000

    it

--- packages/html/src/tokenizer/index.ts
+++ packages/html/src/tokenizer/index.ts
@@ -1,3 +1,4 @@
+import XMLBaseTokenizer from "@obinexuscomputing/xml"
 export type HTMLToken =
   | { type: "Doctype"; value: string; line: number; column: number }
   | { type: "StartTag"; name: string; attributes: Record<string, string>; selfClosing: boolean; line: number; column: number }
