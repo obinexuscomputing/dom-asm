f0a503ca6be47318f6ce357f95c89947ef6664a8
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 20:31:35 +0000

    it

--- packages/xml/jest.config.cjs
+++ packages/xml/jest.config.cjs
@@ -3,17 +3,27 @@ module.exports = {
   testEnvironment: 'node',
   rootDir: '.',
   moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1', // Support path aliases for src
+    '^src/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
-        tsconfig: '<rootDir>/tsconfig.json', // Use the project's tsconfig
+        tsconfig: '<rootDir>/tsconfig.json',
       },
     ],
   },
   testMatch: [
-    '<rootDir>/tests/**/*.test.ts', // Match test files in the `tests` folder
+    '<rootDir>/tests/**/*.test.ts',
   ],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/index.ts',
+    '!src/**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  verbose: true,
+  testTimeout: 60000,
+  maxWorkers: 2
 };
