aa664570600db41b3e8f5fb7991114dadea7c814
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 17:36:26 +0000

    ti

--- packages/html/src/ast/index.ts
+++ packages/html/src/ast/index.ts
@@ -1,7 +1,18 @@
 export interface HTMLASTNode {
   type: "Element" | "Text" | "Comment";
-  name?: string; // Only for type 'Element'
-  value?: string; // Only for type 'Text' or 'Comment'
-  attributes?: Record<string, string>; // Only for type 'Element'
-  children?: HTMLASTNode[]; // Only for type 'Element'
+  name?: string; // For "Element" nodes
+  value?: string; // For "Text" or "Comment" nodes
+  attributes?: Record<string, string>; // For "Element" nodes
+  children?: HTMLASTNode[]; // For "Element" nodes
+}
+
+
+export interface HTMLAST {
+  root: HTMLASTNode; // Root node of the AST
+  metadata?: {
+    nodeCount: number; // Total number of nodes in the AST
+    elementCount: number; // Count of "Element" nodes
+    textCount: number; // Count of "Text" nodes
+    commentCount: number; // Count of "Comment" nodes
+  };
 }
