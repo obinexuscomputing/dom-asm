6f156a503711a95f1f9929f35fca6f0e678d7afb
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 17:40:50 +0000

    it

--- packages/html/dist/optimizer/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { HTMLASTNode } from "../ast";
-export declare class HTMLASTOptimizer {
-    optimize(node: HTMLASTNode): HTMLASTNode;
-    private removeEmptyNodes;
-    private mergeTextNodes;
-}
-//# sourceMappingURL=index.d.ts.map
