9a535660aec0088e6df1ff712d340d90029d306a
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 17:24:02 +0000

    int

--- packages/css/src/index.ts
+++ packages/css/src/index.ts
@@ -1,26 +1,5 @@
-// Re-export modules for public API
-export { Tokenizer } from "./tokenizer";
-export {  ASTNode } from "./ast";
-export { Validator } from "./validator";
-export {  ASTOptimizer } from "./optimizer";
-export { CodeGenerator } from "./generator";
-export { Parser } from "./parser";
-
-
-// Global type declaration for window or global usage
-declare global {
-  interface Window {
-    DOMCSS?: any;
-    DOMHTML?: any;
-    DOMJS?: any;
-  }
-
-  namespace NodeJS {
-    interface Global {
-      DOMCSS?: any;
-      DOMHTML?: any;
-      DOMJS?: any;
-    }
-  }
-
-}
+export * from './ast';
+export * from './parser';
+export * from './tokenizer';
+export * from './validator';
+export * from './generator';
