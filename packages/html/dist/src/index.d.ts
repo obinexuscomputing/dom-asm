3f366368c4cfbe96b26c619d0786a67944767043
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 03:23:15 +0000

    inti

--- packages/html/dist/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { HTMLTokenizer } from "./tokenizer";
-export { AST, ASTNode } from "./ast";
-export { Validator } from "./validator";
-export { ASTOptimizer as Optimizer } from "./optimizer";
-export { CodeGenerator } from "./generator";
-export { Parser, ParserError } from "./parser";
-//# sourceMappingURL=index.d.ts.map
