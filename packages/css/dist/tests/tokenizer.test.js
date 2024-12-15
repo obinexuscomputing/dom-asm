7aa4255ebf30db491dcc3fe0193a5fdfcf18200d
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-15 03:31:03 +0000

    init

---
+++ packages/css/dist/tests/tokenizer.test.js
@@ -0,0 +1,18 @@
+import { Tokenizer } from "../src/tokenizer";
+describe("Tokenizer", () => {
+    test("should tokenize a simple CSS rule", () => {
+        const css = "body { color: black; }";
+        const tokenizer = new Tokenizer(css);
+        const tokens = tokenizer.tokenize();
+        expect(tokens).toEqual([
+            { type: "other", value: "body", position: { line: 1, column: 5 } },
+            { type: "delimiter", value: "{", position: { line: 1, column: 7 } },
+            { type: "other", value: "color", position: { line: 1, column: 13 } },
+            { type: "delimiter", value: ":", position: { line: 1, column: 14 } },
+            { type: "other", value: "black", position: { line: 1, column: 20 } },
+            { type: "delimiter", value: ";", position: { line: 1, column: 21 } },
+            { type: "delimiter", value: "}", position: { line: 1, column: 23 } },
+        ]);
+    });
+});
+//# sourceMappingURL=tokenizer.test.js.map
