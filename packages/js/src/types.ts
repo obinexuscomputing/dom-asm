92803f92e638550b86479f312533fe1da28aff64
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-16 16:16:05 +0000

    ti

--- packages/js/src/types.ts
+++ packages/js/src/types.ts
@@ -19,6 +19,7 @@ export enum NodeType {
   InlineConstant = 'InlineConstant',
   FunctionDeclaration = 'FunctionDeclaration',
   ReturnStatement = 'ReturnStatement',
+  AsyncFunction= 'AsyncFunction'
 }
 
 export enum JSTokenType {
