2a5db57a1fee552772e8d107607f729d960ec1fb
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-16 21:19:24 +0000

    it

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';import { terser } from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
+import { terser } from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts',
