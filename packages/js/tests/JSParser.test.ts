e851dc80cf9a6daa2eb2d8d344799e381797448e
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-16 17:47:02 +0000

    it

--- packages/js/tests/JSParser.test.ts
+++ packages/js/tests/JSParser.test.ts
@@ -8,6 +8,24 @@ describe("JSParser", () => {
   beforeEach(() => {
     parser = new JSParser();
   });
+  test("should parse an empty block statement", () => {
+    const tokens: JSToken[] = [
+        { type: JSTokenType.Delimiter, value: "{" },
+        { type: JSTokenType.Delimiter, value: "}" },
+    ];
+
+    const ast = parser.parse(tokens);
+
+    expect(ast).toEqual({
+        type: NodeType.Program,
+        children: [
+            {
+                type: NodeType.BlockStatement,
+                children: [],
+            },
+        ],
+    });
+});
 
   test("should parse a variable declaration", () => {
     const tokens: JSToken[] = [
@@ -164,4 +182,67 @@ describe("JSParser", () => {
 
     expect(() => parser.parse(tokens)).toThrow("Expected '}' to close block statement");
   });
+  test("should parse an if-else statement", () => {
+    const tokens: JSToken[] = [
+        { type: JSTokenType.Keyword, value: "if" },
+        { type: JSTokenType.Delimiter, value: "(" },
+        { type: JSTokenType.Identifier, value: "x" },
+        { type: JSTokenType.Operator, value: "==" },
+        { type: JSTokenType.Literal, value: "42" },
+        { type: JSTokenType.Delimiter, value: ")" },
+        { type: JSTokenType.Delimiter, value: "{" },
+        { type: JSTokenType.Identifier, value: "doSomething" },
+        { type: JSTokenType.Delimiter, value: "(" },
+        { type: JSTokenType.Delimiter, value: ")" },
+        { type: JSTokenType.EndOfStatement, value: ";" },
+        { type: JSTokenType.Delimiter, value: "}" },
+        { type: JSTokenType.Keyword, value: "else" },
+        { type: JSTokenType.Delimiter, value: "{" },
+        { type: JSTokenType.Identifier, value: "doSomethingElse" },
+        { type: JSTokenType.Delimiter, value: "(" },
+        { type: JSTokenType.Delimiter, value: ")" },
+        { type: JSTokenType.EndOfStatement, value: ";" },
+        { type: JSTokenType.Delimiter, value: "}" },
+    ];
+
+    const ast = parser.parse(tokens);
+
+    expect(ast).toEqual({
+        type: NodeType.Program,
+        children: [
+            {
+                type: NodeType.IfStatement,
+                children: [
+                    {
+                        type: NodeType.BinaryExpression,
+                        value: "==",
+                        children: [
+                            { type: NodeType.Identifier, value: "x" },
+                            { type: NodeType.Literal, value: "42" },
+                        ],
+                    },
+                    {
+                        type: NodeType.BlockStatement,
+                        children: [
+                            {
+                                type: NodeType.Identifier,
+                                value: "doSomething",
+                            },
+                        ],
+                    },
+                    {
+                        type: NodeType.BlockStatement,
+                        children: [
+                            {
+                                type: NodeType.Identifier,
+                                value: "doSomethingElse",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    });
+});
+
 });
