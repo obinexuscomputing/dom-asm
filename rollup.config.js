77cf90ae2cf49143c3a8e0b28f8cfac5df2a9ca8
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-16 21:22:27 +0000

    it

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,17 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { terser } from '@rollup/plugin-terser';
+import pkg from './package.json';
+
+const banner = `/*!
+ * @obinexuscomputing/dom-asm ${pkg.version}
+ * (c) ${new Date().getFullYear()} Obinexus Computing
+ * Released under the ISC License
+ */`;
+
+const footer = `/*!
+ * End of bundle for @obinexuscomputing/dom-asm
+ */`;
 
 export default {
   input: 'src/index.ts',
@@ -11,13 +22,17 @@ export default {
       file: 'dist/index.js',
       format: 'es',
       sourcemap: true,
-      plugins: [terser()], // Minify ES Module
+      plugins: [terser()],
+      banner, // Add banner to ES module
+      footer, // Add footer to ES module
     },
     {
       file: 'dist/index.cjs',
       format: 'cjs',
       sourcemap: true,
-      plugins: [terser()], // Minify CommonJS Module
+      plugins: [terser()],
+      banner, // Add banner to CommonJS module
+      footer, // Add footer to CommonJS module
     },
   ],
   external: [
@@ -37,6 +52,6 @@ export default {
     typescript({
       tsconfig: './tsconfig.json',
     }),
-    terser(), // Add terser plugin
+    terser(), // Global terser plugin for minification
   ],
 };
