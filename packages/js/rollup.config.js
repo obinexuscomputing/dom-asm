531071cbfbdd5d273c82d6b4b5e769621114ab33
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-17 01:53:24 +0000

    init

--- packages/js/rollup.config.js
+++ packages/js/rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import {terser} from '@rollup/plugin-terser';
+
+
 export default [
   // Main build configuration for JavaScript
   {
