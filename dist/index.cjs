b9d657cfe38e9a2e7cc80be844dc1d1482956dc2
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-17 17:57:58 +0000

    it

---
+++ dist/index.cjs
@@ -0,0 +1,12 @@
+/*!
+ * @obinexuscomputing/dom-asm v1.0.0
+ * (c) 2024 OBINexus Computing
+ * Released under the ISC License
+ */
+'use strict';
+
+
+/*!
+ * End of bundle for @obinexuscomputing/dom-asm
+ */
+//# sourceMappingURL=index.cjs.map
---
+++ dist/index.js
@@ -0,0 +1,10 @@
+/*!
+ * @obinexuscomputing/dom-asm v1.0.0
+ * (c) 2024 OBINexus Computing
+ * Released under the ISC License
+ */
+
+/*!
+ * End of bundle for @obinexuscomputing/dom-asm
+ */
+//# sourceMappingURL=index.js.map
