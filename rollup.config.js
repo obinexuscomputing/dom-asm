6ea16624354cdceee945fd5fc1b93fc6c4adb055
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-17 03:55:29 +0000

    ti

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import { terser } from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 
 // Detect if we're in production mode
