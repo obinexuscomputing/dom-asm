3f9f002003aeabf35c15728890946e4998a891bb
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-17 04:02:34 +0000

    it

--- packages/asm/dist/ast/optimizer.d.ts
+++
@@ -1,3 +0,0 @@
-import { ASTNode, MinimizedAutomaton } from "../types";
-export declare function optimizeAST(root: ASTNode, automaton: MinimizedAutomaton): ASTNode;
-//# sourceMappingURL=optimizer.d.ts.map
