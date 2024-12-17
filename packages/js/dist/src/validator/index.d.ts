175a7e607006e611d99f4491959a96fd2f6a64ff
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-17 17:52:24 +0000

    it

--- packages/js/dist/src/validator/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { JSValidator } from './JSValidator';
-export type { ValidationError } from '../types';
-//# sourceMappingURL=index.d.ts.map
--- packages/xml/dist/src/generator/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './DOMXMLGenerator';
-//# sourceMappingURL=index.d.ts.map
