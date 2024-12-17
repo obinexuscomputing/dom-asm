14ef464afe4d6225e6418411ab040fdd60da4c71
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-17 01:14:09 +0000

    iad

---
+++ packages/html/dist/index.d.ts
@@ -0,0 +1,5 @@
+export { HTMLTokenizer } from "./tokenizer/HTMLTokenizer";
+export { HTMLParser } from "./parser/HTMLParser";
+export { HTMLValidator } from "./validator/index";
+export { HTMLCodeGenerator } from "./generator/HTMLCodeGenerator";
+export { HTMLAST, HTMLASTOptimizer } from "./ast/index";
