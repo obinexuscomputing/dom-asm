78e7f1bdda1231db4e775ce3af5f6db1fc0b145e
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-17 04:35:15 +0000

    it

--- packages/css/dist/optimizer/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ASTNode } from "../ast/index";
-export declare class CSSASTOptimizer {
-    private ast;
-    constructor(ast: ASTNode);
-    private removeDuplicateDeclarations;
-    private mergeAdjacentRules;
-    optimize(): ASTNode;
-}
