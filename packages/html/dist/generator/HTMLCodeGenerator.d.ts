1ef17fedd63c30f8ae035a950f6adcd974722441
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-17 01:07:51 +0000

    it

---
+++ packages/html/dist/generator/HTMLCodeGenerator.d.ts
@@ -0,0 +1,8 @@
+import { HTMLASTNode } from "../ast/HTMLAST";
+export declare class HTMLCodeGenerator {
+    private selfClosingTags;
+    constructor(selfClosingTags?: string[]);
+    generateHTML(node: HTMLASTNode): string;
+    private generateAttributes;
+    private isSelfClosingTag;
+}
---
+++ packages/html/dist/validator/index.d.ts
@@ -0,0 +1,1 @@
+export { HTMLValidator } from './HTMLValidator';
