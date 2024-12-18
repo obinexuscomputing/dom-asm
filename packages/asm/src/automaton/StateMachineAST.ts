c3a76d3c16ce809b4585f3c816323c78845d9386
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-18 00:12:21 +0000

    it

--- packages/asm/src/automaton/StateMachineAST.ts
+++ packages/asm/src/automaton/StateMachineAST.ts
@@ -1,24 +1,8 @@
 import { MinimizedAutomaton } from "./StateMinimizer";
 
-export function optimizeAST(root: ASTNode, automaton: MinimizedAutomaton): ASTNode {
-    // Traverse the AST and update transitions based on minimized automaton
-    const updatedNodes = new Map<string, ASTNode>();
-
-    function traverse(node: ASTNode): ASTNode {
-        if (updatedNodes.has(node.id)) return updatedNodes.get(node.id)!;
-
-        const newNode: ASTNode = { ...node, transitions: {} };
-        Object.entries(node.transitions).forEach(([input, nextNodeId]) => {
-            const minimizedState = automaton.transitions[node.id]?.[input];
-            if (minimizedState) {
-                newNode.transitions[input] = traverse({ ...node, id: minimizedState }).id;
-            }
-        });
-        updatedNodes.set(node.id, newNode);
-        return newNode;
-    }
-
-    return traverse(root);
+interface ASTNode {
+    id: string;
+    transitions: { [key: string]: string };
 }
 
 class StateMachineAST {
@@ -51,4 +35,26 @@ class StateMachineAST {
     }
 }
 
-export { StateMachineAST };
+class StateMachineASTOptimizer {
+    public static optimizeAST(root: ASTNode, automaton: MinimizedAutomaton): ASTNode {
+        const updatedNodes = new Map<string, ASTNode>();
+
+        function traverse(node: ASTNode): ASTNode {
+            if (updatedNodes.has(node.id)) return updatedNodes.get(node.id)!;
+
+            const newNode: ASTNode = { ...node, transitions: {} };
+            Object.entries(node.transitions).forEach(([input, nextNodeId]) => {
+                const minimizedState = automaton.transitions[node.id]?.[input];
+                if (minimizedState) {
+                    newNode.transitions[input] = traverse({ ...node, id: minimizedState }).id;
+                }
+            });
+            updatedNodes.set(node.id, newNode);
+            return newNode;
+        }
+
+        return traverse(root);
+    }
+}
+
+export { StateMachineAST, StateMachineASTOptimizer };
